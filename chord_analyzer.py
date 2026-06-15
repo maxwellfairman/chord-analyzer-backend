c8a92ee0b4b11a1d131d09a0c3eaabb530238f74
maxwellfairman <maxwellafairman@gmail.com>
2026-06-15 00:58:44 -0400

    getting rid of the weird min b13 thing

--- chord_analyzer.py
+++ chord_analyzer.py
@@ -277,7 +277,7 @@ def chordQuality(iset: set[str]):
             else:
                 quality = 'min6'
         else:
-            if 'd7' not in iset:
+            if 'd7' not in iset and not (len(iset) < 4 and 'm13' in iset):
                 quality = 'min'
     elif 'M3' in iset:
         if 'm7' in iset:
